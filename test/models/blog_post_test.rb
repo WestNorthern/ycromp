ababc1a48a1d05dc08c96242e24871312d4dde8e
ryan <ryan@admins-MBP.home>
2024-06-30 11:38:01 -0700

    Adds tests for blog post published helper methods

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -2,6 +2,38 @@ require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
   test "draft? returns true for draft blog post" do
-    assert true
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? returns false for a published blog post" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "draft? returns false for a scheduled blog post" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "published? returns true for published blog post" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? returns false for a draft blog post" do
+    refute blog_posts(:draft).published?
+  end
+
+  test "published? returns false for a scheduled blog post" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "scheduled? returns true for scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for a draft blog post" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "scheduled? returns false for a published blog post" do
+    refute blog_posts(:published).scheduled?
   end
 end
