42d063e7e18965c18cbb58d1a964d261b21e5f60
ryan <ryan@admins-MBP.home>
2024-06-30 22:31:39 -0700

    Adds cover_image feature to blog posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -48,7 +48,7 @@ class BlogPostsController < ApplicationController
     end
 
     def blog_post_params
-        params.require(:blog_post).permit(:title, :content, :published_at)
+        params.require(:blog_post).permit(:title, :content, :published_at, :cover_image)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
   # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   # patch "/blog_posts/:id", to: "blog_posts#update"
